7f482bdde121d171e0634dec42ea96ee25b448ac
LazyFatRaccoon <vladimir.todorov.ukraine@gmail.com>
2022-04-25 22:02:42 +0300

    first try

---
+++ js/menu-button.js
@@ -0,0 +1,18 @@
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector('[data-menu-open]'),
+      closeMenuBtn: document.querySelector('[data-menu-close]'),
+      menu: document.querySelector('[data-menu]'),
+      telephone: document.querySelector('[data-menu-telephone]'),
+      email: document.querySelector('[data-menu-email]')
+    };
+  
+    refs.openMenuBtn.addEventListener('click', toggleModal);
+    refs.closeMenuBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.menu.classList.toggle('is-open');
+      refs.telephone.classList.toggle('contacts__element--mobile-telephone');
+      refs.email.classList.toggle('contacts__element--mobile-email');
+    }
+  })();
